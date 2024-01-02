797d7c64a096c7d73ea523a1222b98fc3946acf0
Sam Erde <20478745+SamErde@users.noreply.github.com>
2024-01-02 10:00:49 -0500

    Added more ideas

--- AD Groups/Get-SpecialGroups.ps1
+++ AD Groups/Get-SpecialGroups.ps1
@@ -3,6 +3,10 @@
 
     Get the members of well known security groups by their non-localized SIDs.
 
+    Get count of members in tier-1 groups
+    Check if user [x] is a member of any tier-1 groups
+    Check if user [x] is in a specific tier-1 group or well-known group
+
 #>
 $PrincipalContext = [System.DirectoryServices.AccountManagement.PrincipalContext]::new('domain',$domain)
 $DomainSID = [System.Security.Principal.SecurityIdentifier]::new($domain.GetDirectoryEntry().objectSid.Value,0)
